5f261bf20f9975b0e8794b8f85b19dcaa35d1ed4
[IK] Luis Carlos Colucci <ik.luis@brametal.com.br>
2025-08-01 09:19:29 -0300

    linkbotao

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Hero.css'; // A importação deve corresponder ao nome do arquivo: Hero.css
 
 const Hero = () => {
@@ -7,7 +8,9 @@ const Hero = () => {
       <div className="hero-content">
         <h1 className="hero-title">A Arte que Transcende o Olhar</h1>
         <p className="hero-subtitle">Descubra um universo de cores e formas nas obras de Marcia Santos.</p>
-        <button className="hero-button">Conheça a Galeria</button>
+       <Link to="/galeria" className="hero-button">
+          Conheça a Galeria
+        </Link>
       </div>
     </div>
   );
