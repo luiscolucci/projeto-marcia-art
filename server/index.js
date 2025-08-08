ccbb6a4e15aa551fcd7212616e30f9a533d8eae4
luiscolucci <colucci.luis@gmail.com>
2025-08-08 10:28:35 -0300

    Feat(server): Usa a variável de ambiente PORT para o Cloud Run

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const admin = require('firebase-admin');
-const cors = require('cors'); // 1. IMPORTA A BIBLIOTECA CORS
-const serviceAccount = require('./serviceAccountKey.json');
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors"); // 1. IMPORTA A BIBLIOTECA CORS
+const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 const app = express();
 app.use(cors()); // 2. DIZ AO EXPRESS PARA USAR O CORS E LIBERAR O ACESSO
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; // Ouve na porta definida pelo Cloud Run, ou na 3001 como padrão para ambiente local.
 
-app.get('/', (req, res) => {
-  res.send('Servidor do Projeto Marcia Art está no ar!');
+app.get("/", (req, res) => {
+  res.send("Servidor do Projeto Marcia Art está no ar!");
 });
 
 // NOSSA NOVA ROTA DE API
-app.get('/api/obras', async (req, res) => {
+app.get("/api/obras", async (req, res) => {
   try {
-    const obrasCollection = db.collection('obras');
+    const obrasCollection = db.collection("obras");
     const snapshot = await obrasCollection.get();
 
     if (snapshot.empty) {
@@ -27,21 +27,19 @@ app.get('/api/obras', async (req, res) => {
     }
 
     const obrasList = [];
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       obrasList.push({
         id: doc.id,
-        ...doc.data()
+        ...doc.data(),
       });
     });
 
     res.status(200).json(obrasList);
-
   } catch (error) {
     console.error("Erro ao buscar obras: ", error);
-    res.status(500).send('Erro no servidor ao buscar obras.');
+    res.status(500).send("Erro no servidor ao buscar obras.");
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
